119bae7bcfe26c6f897d1bce54831919d83e9a8a
phpalagu <phpalagu@microsoft.com>
2024-06-24 08:37:29 +0000

    resolving comments

--- GetPathMtu.sh
+++ GetPathMtu.sh
@@ -1,41 +1,70 @@
 #!/bin/bash
 
-ErrorMsg="Initial ping failed"
 
 #################
-#How to use this function
+# 
+# This function will find the optimal MTU between source and destination
+# 
+# How to use this funciton
+# This script can be used with or without flags, see examples for more clarity in usage
+# both options (using with and without flags) are mutual exclusive 
+# This function takes the following parameters
+# Destination IPv4-Address : This is mandatory parameter, we can either use the parameter
+#                            with a flat -s or without a flag
+# Initial Buffer size      : This optional parameter, the default value is 1200bytes
+# Interface Name           : This optional parameter, not using the inteface parameter
+#                            will use the default interface for communication
+# 
 # examples
-# sh GetPathMtu.sh -s <destination-ip> -l <initial-packet-size> -i <interface-name>
-# sh GetPathMtu.sh -s  8.8.8.8 -l 1200 -i eth0
-# note: 
-# 1. -l (Initial packet size) and -s (destination IP-Address) are mandatory arguments
-# 2. give initial packet size (1200 in above example) always a successfull ping packet-size
-# 3. give correct interface name, code failes if interface name is wrong
+# sh GetPathMtu.sh  10.1.0.4  
+# sh GetPathMtu.sh  -r 10.1.0.4 -l 2100 -i eth0
+# sh GetPathMtu.sh  -r 10.1.0.4 -l 1000
+# sh GetPathMtu.sh  -r <destination-ip> -l <initial-packet-size> -i <interface-name>
+# sh GetPathMtu.sh  -r  8.8.8.8 -l 1200 -i eth0
 #################
 
-while getopts s:l:i: flag
+while getopts r:l:i: flag
 do
     case "${flag}" in
-        s) destinationIp=${OPTARG};;
+        r) destinationIp=${OPTARG};;
 
 	l) startSendBufferSize=${OPTARG};;
 
-	i) interfaceName=${OPTARG};;
+        i) interfaceName=${OPTARG};;
+
+	?) echo "Error: Invalid option was specified -$OPTARG"
+	   exit 1;;
     esac
 done
 
+if [ -z "$destinationIp" ]; then
+    destinationIp=$1;
+fi
+
+if [ -z "$startSendBufferSize" ]; then
+    startSendBufferSize=1200;
+fi
+
 echo "destination: $destinationIp"
 echo "startSendBufferSize: $startSendBufferSize"
-echo "interfaceName: $interfaceName"
 
-if [[ -z "$interfaceName" ]]; then
+if [ -z "$interfaceName" ]
+then
+    echo "interfaceName: Default interface"
     initialPingOutput=$(ping -4 -M do -c 1 -s $startSendBufferSize $destinationIp)
 else
+    echo "interfaceName: $interfaceName"
     initialPingOutput=$(ping -4 -M do -c 1 -s $startSendBufferSize $destinationIp -I $interfaceName)
 fi
 
-if [[ $initialPingOutput = *'0 received'* ]]; then
-    echo $ErrorMsg
+if [ $? -ne 0 ]
+then
+    echo "Exiting the code, error occured in intial ping"
+    exit 1
+fi
+
+if [[ $initialPingOutput == *"0 received"* ]]; then
+    echo "Initial ping failed"
     echo "Initial ping should be successfull, check Destination-IP or lower initial size"
     exit 1
 fi
@@ -53,8 +82,9 @@ while [ $tempPassedBufferSize -ne $sendBufferSize ]; do
     successfullBufferSize=$sendBufferSize
     while [ true ]; do
 	
-        if [[ -z "$interfaceName" ]]; then
-            ping -4 -M do -c 1 -s $tempSendBufferSize $destinationIp &> /dev/null
+        if [ -z "$interfaceName" ]
+	then
+            ping -4 -M do -c 1 -s $tempSendBufferSize $destinationIp &> /dev/null 
         else
             ping -4 -M do -c 1 -s $tempSendBufferSize $destinationIp -I $interfaceName &> /dev/null
 	fi 
